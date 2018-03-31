244ac927c0e66123baba1618244efd6fe714a172
Jelena Gajic <jelenagajic1802@gmail.com>
2018-03-31 20:53:02 +0200

    Optimize tabs.js

--- tabs.js
+++ tabs.js
@@ -2,36 +2,78 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // add defult class to home page
     document.getElementById('home').classList.add('active');
+
+    // get navigation items
+    var home = document.getElementById('home');
+    var about = document.getElementById('about');
+    var contact = document.getElementById('contact');
+
+    // get individual content
     var homeContent = document.getElementById('home_content');
     var contactContent = document.getElementById('contact_content');
     var aboutContent = document.getElementById('about_content');
 
-    document.getElementById('contact').onclick = function() {
-        var content = document.getElementById('contact_content');
-        homeContent.style.display = 'none';
-        document.getElementById('home').classList.remove('active');
-        aboutContent.style.display = 'none';
-        document.getElementById('about').classList.remove('active');
-        content.style.display = 'block';
-        this.classList.add('active');
+    // changing tabs
+    function changeTabs (hideEl, hideAnotherEl, showEl, rmClassTab, rmClassTabAnohter) {
+        hideEl.style.display = 'none';
+        hideAnotherEl.style.display = 'none';
+        showEl.style.display = 'block';
+        rmClassTab.classList.remove('active');
+        rmClassTabAnohter.classList.remove('active');
+        // this.classList.add('active');
     }
-    document.getElementById('home').onclick = function() {
-        var content = document.getElementById('home_content');
-        contactContent.style.display = 'none';
-        document.getElementById('contact').classList.remove('active');
-        aboutContent.style.display = 'none';
-        document.getElementById('about').classList.remove('active');
 
-        content.style.display = 'block';
+    // add event listeners
+    contact.addEventListener('click', function() {
+        changeTabs(homeContent, aboutContent, contactContent, home, about);
         this.classList.add('active');
-    }
-    document.getElementById('about').onclick = function() {
-        var content = document.getElementById('about_content');
-        contactContent.style.display = 'none';
-        document.getElementById('contact').classList.remove('active');
-        homeContent.style.display = 'none';
-        document.getElementById('home').classList.remove('active');;
-        content.style.display = 'block';
+    });
+
+    home.addEventListener('click', function() {
+        changeTabs(contactContent, aboutContent, homeContent, contact, about);
         this.classList.add('active');
-    }
+
+    });
+
+    about.addEventListener('click', function() {
+        changeTabs(contactContent, homeContent, aboutContent, home, contact);
+        this.classList.add('active');
+    });
+
+
+    // contact.onclick = function() {
+    //     // elements for hiding
+    //     homeContent.style.display = 'none';
+    //     aboutContent.style.display = 'none';
+    //     // show content
+    //     contactContent.style.display = 'block';
+    //     // remove active border
+    //     home.classList.remove('active');
+    //     about.classList.remove('active');
+    //     // add active
+    //     this.classList.add('active');
+    // }
+    // home.onclick = function() {
+    //     // hide elements
+    //     contactContent.style.display = 'none';
+    //     aboutContent.style.display = 'none';
+    //     // show element
+    //     home_content.style.display = 'block';
+    //     // remove active border
+    //     contact.classList.remove('active');
+    //     about.classList.remove('active');
+    //     // add active
+    //     this.classList.add('active');
+    // }
+    // about.onclick = function() {
+    //     // hide
+    //     contactContent.style.display = 'none';
+    //     homeContent.style.display = 'none';
+    //     // show
+    //     aboutContent.style.display = 'block';
+    //     // remove
+    //     contact.classList.remove('active');
+    //     home.classList.remove('active');;
+    //     this.classList.add('active');
+    // }
 });
